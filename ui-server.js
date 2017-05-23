ac54ba50db534b8b95198ac04a0576ea2dfeabe7
Jason W <jaw977@yahoo.com>
2017-05-23 06:09:50 -0400

    Procfile

--- ui-server.js
+++ ui-server.js
@@ -1,6 +1,6 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
 
 const configJSON = JSON.stringify({apiURL:process.env.API_URL});
 
