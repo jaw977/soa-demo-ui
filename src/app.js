083bc9b8ec04fff4d12e8f4e8d7a3da72cd9dd0a
Jason W <jaw977@yahoo.com>
2017-07-30 23:09:07 -0400

    Convert bid history component to .vue file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ajaxPromise from './ajax_promise.js';
-import VueBidHistory from './bid_history.js';
+import VueBidHistory from './bid_history.vue';
 
 Vue.use(VueRouter);
 
